865ed67092343d699c67d0494bb2916fba67e4ff
Alexssandro <22282169@CFP127ALUNO.LOCAL>
2022-09-01 16:54:25 -0300

    primeiro teste

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -1,8 +1,17 @@
 package br.senai.sp.jandira;
 
+import br.senai.sp.jandira.model.Multiplicacao;
+
 public class AppTabuada {
 	
 	public static void main(String[] args) {
+		Multiplicacao m = new Multiplicacao();
+		
+		m.multiplicando = 4;
+		m.minMultiplicador = 1;
+		m.maxMultiplicador = 9;
+		
+		m.getTabuada();
 		
 	}
 	
--- src/br/senai/sp/jandira/model/Multiplicacao.java
+++ src/br/senai/sp/jandira/model/Multiplicacao.java
@@ -5,5 +5,13 @@ public class Multiplicacao {
 	public int multiplicando;
 	public int minMultiplicador;
 	public int maxMultiplicador;
-	
+	 	
+	public void getTabuada() {
+		while (minMultiplicador <= maxMultiplicador) {
+			
+			int res = multiplicando * minMultiplicador;
+			System.out.println(multiplicando + "X" + minMultiplicador + "=" + res);
+			minMultiplicador++;
+		}
+	}
 }
